f5c0fc8a0f627548665c61b5546c5451c1279b65
sp1v4k <tonifrost@gmail.com>
2016-04-04 18:44:23 +0200

    comments everything but sort method

--- collection_methods.rb
+++ collection_methods.rb
@@ -20,37 +20,64 @@ homes =[
 
 #collection index
 #each
-homes.each do |hm|
-  puts "#{hm.name} in #{hm.city}\n Price: #{hm.price} a night"
+#homes.each do |hm|
+#  puts "#{hm.name} in #{hm.city}\n Price: #{hm.price} a night"
+#end
+#
+#
+#names = []
+#
+##new array creation from Object array elements
+#homes.each { |hm| names << hm.name  }
+#puts names
+#
+##map method
+##it is normal to do it with map method
+#cities = []
+#cities = homes.map { |hm| hm.city }
+#puts cities
+#
+##each_with_index: same as each but the block recieves a second parameter, the index
+#
+#homes.each_with_index do |hm, index|
+#  puts "#{hm.name} is home number: #{index + 1}"
+#end
+#
+##select collector method: filter the object array and creates a new one. Do not touch original. Discard all different to the item passed.
+#
+#san_juan_homes = []
+#san_juan_homes = homes.select do |hm|
+#  #keeps hm if city is San Juan
+#  hm.city == "San Juan"
+#end
+#
+#san_juan_homes.each do |hm|
+#  puts "#{hm.name} in #{hm.city}\n Price: #{hm.price} a night"
+#end
+#
+#high_capacity_homes = []
+#high_capacity_homes = homes.select do |hm|
+#  hm.capacity >= 4
+#end
+#
+#
+#high_capacity_homes.each do |hm|
+#  puts "#{hm.name} in #{hm.city}\n Price: #{hm.price} a night"
+#end
+##find collector method: finds one item and return that item. ONLY DE FIRST ONE!
+#
+#home_41_dollars = homes.find do |hm|
+#  hm.price == 41
+#end
+#
+#puts home_41_dollars.name
+
+sorted_homes = []
+
+sorted_homes = homes.sort do |home1, home2|
+  home1.price <=> home2.price
 end
 
-
-names = []
-
-#new array creation from Object array elements
-homes.each { |hm| names << hm.name  }
-puts names
-
-#map method
-#it is normal to do it with map method
-cities = []
-cities = homes.map { |hm| hm.city }
-puts cities
-
-#each_with_index: same as each but the block recieves a second parameter, the index
-
-homes.each_with_index do |hm, index|
-  puts "#{hm.name} is home number: #{index + 1}"
-end
-
-#select collector method: filter the object array and creates a new one. Do not touch original. The block return true or false given an item.
-
-san_juan_homes = []
-san_juan_homes = homes.select do |hm|
-  #keeps hm if city is San Juan
-  hm.city == "San Juan"
-end
-
-san_juan_homes.each do |hm|
+sorted_homes.each do |hm|
   puts "#{hm.name} in #{hm.city}\n Price: #{hm.price} a night"
 end
