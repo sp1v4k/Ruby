16b670824c4ac656ab9d1699e0f5ac7cc3c4b17d
sp1v4k <tonifrost@gmail.com>
2016-04-07 20:04:18 +0200

    Adds sort by age method

--- the_premise_exercise.rb
+++ the_premise_exercise.rb
@@ -8,19 +8,27 @@ class ProgrammingLanguage
     @age = age
     @type = type
   end
-
 end
 
 class List
   attr_reader :list_language
 
-  def initialize(list_language)
-    @list_language = list_language
+  def to_list(list_language)
+    list_language.each do |pl|
+      puts "Name: #{pl.name} Age: #{pl.age}  Type: #{pl.type}"
+    end
   end
 
-  def to_list
-    @list_language.each do |pl|
-      puts "Name: #{pl.name} Age: #{pl.age}  Type: #{pl.type}"
+  def age_update(list_language)
+    list_language.map do |pl|
+      pl.age += 1
+      pl
+    end
+  end
+
+  def sort_by_creation(list_language)
+    list_language.sort do |pl, pl1|
+      pl.age <=> pl1.age
     end
   end
 end
@@ -35,5 +43,9 @@ swift = ProgrammingLanguage.new("Swift", 2, "static")
 java = ProgrammingLanguage.new("Java", 20, "static")
 
 programming_languages = [ruby, python, javascript, go, rust, swift, java]
-list = List.new(programming_languages)
-list.to_list
+list = List.new
+list.to_list(programming_languages)
+#list_updated = list.age_update(programming_languages)
+#list.to_list(list_updated)
+list_sorted = list.sort_by_creation(programming_languages)
+list.to_list(list_sorted)
